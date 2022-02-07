df45efdfc5d7042e0556c54e1464425ad70f7aa4
MediaBay <med.bay@yandex.ru>
2022-02-07 22:48:58 +0300

    index text and link section

---
+++ dist/template-parts/index-text-link.php
@@ -0,0 +1,4 @@
+<section class="index-lf container sect-bg">
+  <h2 class="index-lf__title sect-title"><?php echo $section['text'] ?></h2>
+  <a href="<?php echo $section['link']['url'] ?>" class="index-lf__link btn btn-ol-white"><?php echo $section['link']['title'] ?></a>
+</section>
