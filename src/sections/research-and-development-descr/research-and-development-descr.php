01276959d2a5ef130837f66202f2b021dce49468
MediaBay <med.bay@yandex.ru>
2022-02-08 15:39:06 +0300

    r&d descr section

---
+++ src/sections/research-and-development-descr/research-and-development-descr.php
@@ -0,0 +1,4 @@
+<section class="rd-descr container sect">
+  <h2 class="rd-descr__title sect-title"><?php echo $section['title'] ?></h2>
+  <p class="rd-descr__text"><?php echo $section['descr'] ?></p>
+</section>
